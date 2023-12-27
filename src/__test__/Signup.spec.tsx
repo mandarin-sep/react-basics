6b74deaf6af4212144b757583370a4c5bf04df5f
mandarin-sep <vheh0901@gmail.com>
2023-12-27 11:01:40 +0900

    feat: password Testing

--- src/__test__/Signup.spec.tsx
+++ src/__test__/Signup.spec.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import SignupPage from "../pages/SignupPage";
 import { RouterProvider, createMemoryRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -9,7 +9,7 @@ const queryClient = new QueryClient({
 });
 
 describe("회원가입 테스트", () => {
-  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다", () => {
+  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다", async () => {
     // given - 회원가입 페이지가 그려짐
     const routes = [
       {
@@ -30,6 +30,22 @@ describe("회원가입 테스트", () => {
     );
 
     // when - 비밀번호와 비밀번호 확인 값이 일치하지 않음
+
+    //signUp 페이지에 label 태그에 작성된 text를 이용해서 컴포넌트를 가져옴
+    const passwordInput = screen.getByLabelText("비밀번호");
+    const confirmPasswordInput = screen.getByLabelText("비밀번호 확인");
+
+    //이벤트를 발생시킨다
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.change(confirmPasswordInput, {
+      target: { value: "wrongPassword" },
+    });
+
     // then - 에러메세지가 표시됨
+
+    //signUp 페이지에 존재하는 data-testid 속성에 erroe-message 라는 값이 들어간 컴포넌트를 찾아서 이런 메세지가 나오는지 확인
+    // await를 쓰는 이유는 해당값이 나오는지 확인해야 하기 때문
+    const errorMessage = await screen.findByTestId("error-message");
+    expect(errorMessage).toBeInTheDocument();
   });
 });
